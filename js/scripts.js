e0b6f378307c0662f427dacba2db620c81fd9c7a
Edgar Gonzales <egonzales@A99.local>
2018-04-13 17:44:08 +0800

    Initial

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+( function() {
+
+	
+	jQuery(document).ready( function() {
+
+		new WOW().init();
+
+		jQuery('#fullpage').fullpage({
+			navigation: true,
+			autoScrolling:false
+		})
+		
+		
+	});
+	
+	jQuery(window).on('load', function(){
+
+
+	})
+
+
+})();
