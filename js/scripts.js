de06e198a89eb17c08eef6188c17aa6a02214270
EdgarWill <edgarwilliam1692@live.com>
2018-04-14 22:46:59 +0800

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,22 @@
 ( function() {
 
-	
+	function afterReveal (el) {
+		el.addEventListener('animationend', function () {
+			jQuery('.wow').addClass('animation-fired');
+		});
+	}
+
+
+
 	jQuery(document).ready( function() {
 
-		new WOW().init();
+		new WOW({ callback: afterReveal }).init();
 
 		jQuery('#fullpage').fullpage({
 			navigation: true,
 			autoScrolling:false
 		})
-		
-		
+			
 	});
 	
 	jQuery(window).on('load', function(){
